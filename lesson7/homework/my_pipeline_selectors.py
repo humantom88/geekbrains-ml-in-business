b722b0c69f3afd91ca47a0c46688ee083b4774a2
Artyom Belov <humantom88@gmail.com>
2021-03-28 11:04:08 +0300

    lesson7 in progress

---
+++ lesson7/homework/my_pipeline_selectors.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from sklearn.base import BaseEstimator, TransformerMixin
+
+class FeatureSelector(BaseEstimator, TransformerMixin):
+    def __init__(self, column):
+        self.column = column
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X, y=None):
+        return X[self.column]
+    
+class NumberSelector(BaseEstimator, TransformerMixin):
+    """
+    Transformer to select a single column from the data frame to perform additional transformations on
+    Use on numeric columns in the data
+    """
+    def __init__(self, key):
+        self.key = key
+
+    def fit(self, X, y=None):
+        return self
+
+    def transform(self, X):
+        return X[[self.key]]
+    
+class OHEEncoder(BaseEstimator, TransformerMixin):
+    def __init__(self, key):
+        self.key = key
+        self.columns = []
+
+    def fit(self, X, y=None):
+        self.columns = [col for col in pd.get_dummies(X, prefix=self.key).columns]
+        return self
+
+    def transform(self, X):
+        X = pd.get_dummies(X, prefix=self.key)
+        test_columns = [col for col in X.columns]
+        for col_ in self.columns:
+            if col_ not in test_columns:
+                X[col_] = 0
+        return X[self.columns]
